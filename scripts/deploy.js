2165edcbcc27bfcf912d742c0e7a383031fa6326
ZuperZee <zuperpenguin@gmail.com>
2020-10-04 04:43:59 +0200

    Fix wrong publish directory

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,7 @@
 const ghpages = require("gh-pages");
 
 ghpages.publish(
-  "__sapper__/export",
+  "__sapper__/export/svelte-dev",
   {
     branch: "master",
     repo: "https://github.com/ZuperZee/sapper-dev.git",
